375dfdc9fa55e9dc79924f9c82c08d91b7947fb0
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2022-04-11 20:08:33 +0900

    ie

---
+++ bacha/bacha.2022.04.06/2.cpp
@@ -0,0 +1,26 @@
+#define rep(i, n) for (ll i = 0; i < (ll)(n); ++i)
+#define reps(i, s, n) for (int i = (s); i < (int)(n); ++i)
+#define rrep(i, n) for (int i = (int)(n-1); i >= 0; --i)
+#define MOD 1000000007
+#define INF 2000000000
+#define INFL 1000000000000000000
+template <typename T> bool chmin(T &a, const T& b) { if (a > b) { a = b; return true; } return false; }
+template <typename T> bool chmax(T &a, const T& b) { if (a < b) { a = b; return true; } return false; }
+template <typename T> int count_digit(T n) { int res = 0; while (n > 0) { ++res; n /= 10; } return res; }
+typedef long long ll;
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long a, b, c;
+    cin >> a >> b >> c;
+    string ans = "NO";
+    rep (i, 1000) {
+        if (a*i % b == c) {
+            ans = "YES";
+            break;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
---
+++ bacha/bacha.2022.04.11/9.cpp
@@ -0,0 +1,26 @@
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define reps(i, s, n) for (int i = (s); i < (int)(n); ++i)
+#define rrep(i, n) for (int i = (int)(n-1); i >= 0; --i)
+#define MOD 1000000007
+#define INF 2000000000
+#define INFL 1000000000000000000
+template <typename T> bool chmin(T &a, const T& b) { if (a > b) { a = b; return true; } return false; }
+template <typename T> bool chmax(T &a, const T& b) { if (a < b) { a = b; return true; } return false; }
+template <typename T> int count_digit(T n) { int res = 0; while (n > 0) { ++res; n /= 10; } return res; }
+typedef long long ll;
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long h, w;
+    cin >> h >> w;
+    vector<vector<bool>> s(h, vector<bool>(w));
+    rep (i, h) rep (j, w) {
+        char tmp;
+        cin >> tmp;
+        s[i][j] = (tmp == '.' ? true : false);
+    }
+
+    
+    return 0;
+}
