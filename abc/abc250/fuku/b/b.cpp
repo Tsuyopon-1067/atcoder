797e46685e5a86dffdfe5e9060e22bafe4f66c19
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2022-05-14 20:28:06 +0900

    ie

---
+++ abc/abc250/fuku/b/b.cpp
@@ -0,0 +1,26 @@
+#define rep(i, n) for (long long i = 0; i < (long long)(n); ++i)
+#define reps(i, s, n) for (long long i = (long long)(s); i < (long long)(n); ++i)
+#define rrep(i, n) for (long long i = (long long)(n-1); i >= 0; --i)
+#define MOD 1000000007
+#define INF 2000000000
+#define INFL 1000000000000000000
+template <typename T> bool chmin(T &a, const T& b) { if (a > b) { a = b; return true; } return false; }
+template <typename T> bool chmax(T &a, const T& b) { if (a < b) { a = b; return true; } return false; }
+template <typename T> long long count_digit(T n) { long long res = 0; while (n > 0) { ++res; n /= 10; } return res; }
+typedef long long ll;
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    long long n, a, b;
+    cin >> n >> a >> b;
+    
+    rep (i, n) {
+        string line = "";
+        rep (j, n) {
+            char c = ((i+j)%2 == 0 ? '.' : '#');
+            rep (k, b) line += c;
+        }
+        rep (j, a) cout << line << endl;
+    }
+    return 0;
+}
