a724f335a201c9dbf409a2347f561fb99431c557
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2022-07-05 15:45:02 +0900

    delete out

--- abc/abc212/C.cpp
+++ abc/abc212/C.cpp
@@ -1,40 +1,26 @@
-#define _GLIBCXX_DEBUG
-#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define INF 2000000000;
+template <typename T> bool chmin(T &a, const T& b) { if (a > b) { a = b; return true; } return false; }
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	int n, m;
-	cin >> n >> m;
-	vector<int> a(n);
-	vector<int> b(n);
-	for(int i = 0; i < n; i++) cin >> a[i];
-	for(int i = 0; i < m; i++) cin >> b[i];
-	sort(a.begin(), a.end());
-	sort(b.begin(), b.end());
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n);
+    vector<int> b(m);
+    rep (i, n) cin >> a[i];
+    rep (i, m) cin >> b[i];
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
 
-	int i = 0;
-	int j = 0;
-	int ans = abs(a[0] - b[0]);
-	rep(i, n){
-		do{
-			ans = min(ans, abs(a[i]-b[j]));
-			printf("a = %d, b = %d\n", a[i], b[j]);
-			j++;
-			if(j == m){
-				i = n;
-				break;
-			}
-		}while(ans >= abs(a[i]-b[j]));
-		do{
-			ans = min(ans, abs(a[i]-b[j]));
-			printf("a = %d, b = %d\n", a[i], b[j]);
-			i++;
-			if(i == n){
-				break;
-			}
-		}while(ans >= abs(a[i]-b[j]));
-	}
-	cout << ans << endl;
-	return 0;
+    int ans = INF;
+    int ai = 0, bi = 0;
+    while (ai < n && bi < m) {
+        chmin(ans, abs(a[ai] - b[bi]));
+        if (a[ai] < b[bi]) ++ai;
+        else ++bi;
+    }
+    cout << ans << endl;
+    return 0;
 }
