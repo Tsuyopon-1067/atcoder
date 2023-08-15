e8fa5486326abfe851680811f18b5f2acc822926
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-08-16 00:40:03 +0900

    abc271

---
+++ abc/abc271/b.py
@@ -0,0 +1,9 @@
+n, q = map(int, input().split())
+a = []
+for i in range(n):
+    tmp = list(map(int, input().split()))
+    a.append(tmp[1:])
+
+for i in range(q):
+    s, t = map(int, input().split())
+    print(a[s-1][t-1])
